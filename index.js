5b82cca06e1b16d86d53c78d7473bf16e6ccbc14
Faiq Ali <faiqalii19@gmail.com>
2021-07-24 02:22:26 +0500

    Router all

--- index.js
+++ index.js
@@ -3,22 +3,26 @@ const http = require('http');
 const morgan = require('morgan');
 //const bodyParser = require('body-parser');
 const dishRouter = require('./routes/dishRouter');
-const dishIDRouter = require('./routes/dishIDRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+
 app.use(morgan('dev'));
-
-//app.use(bodyParser.json());
-
-app.use('/dishes', dishRouter);
-app.use('/dishes/:dishID' , dishIDRouter);
-
 app.use(express.static(__dirname + '/public'));
+// app.use(bodyParser.json());
+app.use(express.json());
+
+app.use('/dishes' , dishRouter);
+app.use('/promotions' , promoRouter);
+app.use('/leader' , leaderRouter);
+
 
 app.use((req, res, next) => {
+    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
